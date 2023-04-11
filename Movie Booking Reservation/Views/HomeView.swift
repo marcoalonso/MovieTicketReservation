910e6d47e2ad0ce092556b483b83ec4032397ce3
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-04-11 16:55:44 -0600

    Scroll Section added to see like a collectionView

--- Movie Booking Reservation/Views/HomeView.swift
+++ Movie Booking Reservation/Views/HomeView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct HomeView: View {
     @State var animate = false
     
+    @State var posters1: [String] = ["poster1", "poster2", "poster3", "poster4", "poster5", "poster6"]
+
+    @State var posters2: [String] = ["poster7", "poster8", "poster9", "poster10", "poster11", "poster12"]
+
+    @State var posters3: [String] = ["poster13", "poster14", "poster15", "poster16", "poster17", "poster18"]
+    
     var body: some View {
         ZStack {
             CircleBackground(color: Color("greenCircle"))
@@ -33,6 +39,16 @@ struct HomeView: View {
                 
                 CustomSearchBar()
                     .padding(EdgeInsets(top: 30, leading: 20, bottom: 20, trailing: 20))
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack {
+                        ScrollSection(title: "Now Playing", posters: posters1)
+                        ScrollSection(title: "Coming soon", posters: posters2)
+                        ScrollSection(title: "Top Movies", posters: posters3)
+                        ScrollSection(title: "Favorite", posters: posters1)
+                    }
+                    .padding(.bottom, 90)
+                }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         } .background(
